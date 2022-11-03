962be0123111ba2884c56df6eb7c12ff6b6e353a
Gabriel Arcila <gabriel_jesus1122@hotmail.com>
2022-11-03 08:53:59 -0400

    Introduccion del segundo turno del gespline

--- Data/API.cs
+++ Data/API.cs
@@ -7,7 +7,9 @@ namespace LibroNovedades.Data.API
     {
         Task<List<List<string>>> obtenerParadasActuales1turnoPorLinea(string centroCosto);
         Task<List<List<string>>> obtenerParadasActuales1turnoPorLinea(string centroCosto,List<LibroNove> listaNove);
+        Task<List<List<string>>> obtenerParadasActualesturnoPorLinea(string centroCosto,List<LibroNove> listaNove);
         Task<Dictionary<string,string>>? obtenerUsuario(string ficha);
+
         Task<List<string>>? ObtenerTurnoYGrupo();
     }
 
@@ -16,7 +18,15 @@ namespace LibroNovedades.Data.API
         private HttpClient cliente;
         public async Task<List<List<string>>>? obtenerParadasActuales1turnoPorLinea(string centroCosto){
             List<List<string>> data;
-            string url = "http://operaciones.papeleslatinos.com/neoapi/OEE/obtenerParadasActuales1turnoPorLinea/" + centroCosto;
+            string url = "http://operaciones.papeleslatinos.com/neoapi/gespline/obtenerParadasActuales1turnoPorLinea/" + centroCosto;
+            this.cliente = new HttpClient();
+            data = await cliente.GetFromJsonAsync<List<List<string>>>(url);
+            return data;
+        }
+
+        public async Task<List<List<string>>>? obtenerParadasActuales2turnoPorLinea(string centroCosto){
+            List<List<string>> data;
+            string url = "http://operaciones.papeleslatinos.com/neoapi/gespline/ObtenerParadasSegundoTurnoPorMaquina/" + centroCosto;
             this.cliente = new HttpClient();
             data = await cliente.GetFromJsonAsync<List<List<string>>>(url);
             return data;
@@ -25,22 +35,52 @@ namespace LibroNovedades.Data.API
             string ParadasIgnorar = "[";
             List<List<string>> data = new List<List<string>>();
             this.cliente = new HttpClient();
-            if(listaNove.Count == 0){
+            if(listaNove.Count == 0 || listaNove[0].IdParada == null){
                 return await this.obtenerParadasActuales1turnoPorLinea(centroCosto);
             }else{
                 for (int i = 0; i < listaNove.Count; i++)
                 {
-                    if(i ==  listaNove.Count-1){
-                        ParadasIgnorar += listaNove[i].IdParaGesp.ToString().Substring(1);
+                    if(i ==  listaNove.Count - 1){
+                        ParadasIgnorar += listaNove[i].IdParada.ToString().Substring(1);
                     }else{
-                        ParadasIgnorar +=  listaNove[i].IdParaGesp.ToString().Substring(1) + ",";
+                        ParadasIgnorar +=  listaNove[i].IdParada.ToString().Substring(1) + ",";
                     }
                 }
                 ParadasIgnorar += "]";
             }
-            string url = "http://operaciones.papeleslatinos.com/neoapi/OEE/obtenerParadasActuales1turnoPorLinea/" + centroCosto + "/" + ParadasIgnorar;
+            string url = "http://operaciones.papeleslatinos.com/neoapi/gespline/obtenerParadasActuales1turnoPorLinea/" + centroCosto + "/" + ParadasIgnorar;
             data = await cliente.GetFromJsonAsync<List<List<string>>>(url);     
             return data;
+        }
+
+        public async Task<List<List<string>>>? obtenerParadasActuales2turnoPorLinea(string centroCosto, List<LibroNove> listaNove){
+            string ParadasIgnorar = "[";
+            List<List<string>> data = new List<List<string>>();
+            this.cliente = new HttpClient();
+            if(listaNove.Count == 0 || listaNove[0].IdParada == null){
+                return await this.obtenerParadasActuales2turnoPorLinea(centroCosto);
+            }else{
+                for (int i = 0; i < listaNove.Count; i++)
+                {
+                    if(i ==  listaNove.Count - 1){
+                        ParadasIgnorar += listaNove[i].IdParada.ToString().Substring(1);
+                    }else{
+                        ParadasIgnorar +=  listaNove[i].IdParada.ToString().Substring(1) + ",";
+                    }
+                }
+                ParadasIgnorar += "]";
+            }
+            string url = "http://operaciones.papeleslatinos.com/neoapi/gespline/ObtenerParadasSegundoTurnoPorMaquina/" + centroCosto + "/" + ParadasIgnorar;
+            data = await cliente.GetFromJsonAsync<List<List<string>>>(url);     
+            return data;
+        }
+        public async Task<List<List<string>>>? obtenerParadasActualesturnoPorLinea(string centroCosto, List<LibroNove> listaNove){
+            DateTime tiempo =  DateTime.Now;
+            if(tiempo.Hour >= 6 && tiempo.Hour < 18){
+                return await this.obtenerParadasActuales1turnoPorLinea(centroCosto,listaNove);
+            }else{
+                return await this.obtenerParadasActuales2turnoPorLinea(centroCosto,listaNove);
+            }
         }
         public async Task<Dictionary<string,string>>? obtenerUsuario(string ficha){
             Dictionary<string,string> usuario = new Dictionary<string,string>();
--- Models/LibroNove.cs
+++ Models/LibroNove.cs
@@ -8,17 +8,14 @@ namespace LibroNovedades.Models
     public partial class LibroNove
     {
         public int IdReuDia { get; set; }
-
         [ValidDiferenteACero]
         public int IdLinea { get; set; }
         [Required(ErrorMessage ="Coloque el id de equipo afectado.")]
         public string IdEquipo { get; set; } = null!;
         [Required(ErrorMessage ="Coloque la novedad.")] 
         public string Rddiscrepa { get; set; } = null!;
-
         [ValidDiferenteACero]
         public double RdtiePerMi { get; set; }
-
         [Required(ErrorMessage ="Coloque su ficha.")]
         public string RdfichaRes { get; set; } = null!;
         public DateTime Rdfecha { get; set; }
@@ -26,24 +23,18 @@ namespace LibroNovedades.Models
         public string Rdgrupo { get; set; } = null!;
         [ValidDiferenteACero]
         public string RdTurno { get; set; } = null!;
-
         [Required(ErrorMessage ="Coloque el id de la maquina.")]
         public string IdMaquina { get; set; } = null!;
-        
         [ValidDiferenteACero]
         public int IdTipoNove { get; set; }
-
         [Required(ErrorMessage ="Coloque el area a la que pertenece.")]
         public int IdAreaCar { get; set; }
-
-        [ValidDiferenteACero]
-        public int IdParaGesp { get; set; }
-
         [Required(ErrorMessage ="Coloque la accion correspondiente.")]
         public string Rdaccion { get; set; } = null!;
+        public int? IdParada { get; set; }
+
         public virtual Area IdAreaCarNavigation { get; set; } = null!;
         public virtual Linea IdLineaNavigation { get; set; } = null!;
         public virtual TiParTp IdTipoNoveNavigation { get; set; } = null!;
-        
     }
 }

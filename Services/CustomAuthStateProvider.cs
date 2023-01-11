73c56cbb85a873a78050e02e6e3268600dcb9a04
Gabriel Arcila <gabriel_jesus1122@hotmail.com>
2023-01-11 15:56:57 -0400

    Login en Libro de Novedades

--- Services/CustomAuthStateProvider.cs
+++ Services/CustomAuthStateProvider.cs
@@ -24,7 +24,7 @@ namespace LibroNovedades.Service.Autenticacion
 
         public override async  Task<AuthenticationState>  GetAuthenticationStateAsync()
         {
-            string token = await _localStorage.GetItemAsStringAsync("Autocontrol De Calidad");
+            string token = await _localStorage.GetItemAsStringAsync("LibroDeNovedades");
 
             var identity = new ClaimsIdentity();
             _http.DefaultRequestHeaders.Authorization = null;

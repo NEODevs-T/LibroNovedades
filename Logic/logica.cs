d5d92d489afbb82f0e701bcd8f84371d678c3ba0
Gabriel Arcila <gabriel_jesus1122@hotmail.com>
2023-08-11 10:27:43 -0400

    Solucion de error

--- Logic/logica.cs
+++ Logic/logica.cs
@@ -92,7 +92,7 @@ namespace LibroNovedades.Logic{
             }
             if(listaPizarra.Count > 0){
                 //TOD: Descomentar
-                // dataChismoso.InsertarRegistros(ListaChismosoCambioFecha,ListaChismosoCambioEstado);
+                dataChismoso.InsertarRegistros(ListaChismosoCambioFecha,ListaChismosoCambioEstado);
                 return Tuple.Create(true,listaNovedadesFiltrada);
             }else{
                 return Tuple.Create(false,listaNovedadesFiltrada);

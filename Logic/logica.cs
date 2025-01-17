590d304e1c98dde82fc06ce551eef2006b8aa45f
Geohandry Nuñez <147347049+geohaXD@users.noreply.github.com>
2025-01-17 10:25:04 -0400

    cambio de string OrigenCal implentando condicional

--- Logic/logica.cs
+++ Logic/logica.cs
@@ -105,7 +105,6 @@ namespace LibroNovedades.Logic
                         }
                         //TODO: Revisar cambio para poder ubicar al nombre del centro
                         registroNuevo.IdTipReu = 2;
-                        registroNuevo.OrigenCal = "N/A";
                         registroNuevo.RdcodEq = temporal.IdEquipo;
                         registroNuevo.Rddisc = temporal.Lndiscrepa;
                         registroNuevo.RdfecReu = DateTime.Now;
@@ -122,9 +121,11 @@ namespace LibroNovedades.Logic
                         if(TPM == 4 || IdTipoNove == 8)
                         {
                             registroNuevo.IdCausaCal = 2;
+                            registroNuevo.OrigenCal = " ";
                         }else
                         {
                             registroNuevo.IdCausaCal = 1;
+                            registroNuevo.OrigenCal = "N/A";
                         }
                         listaPizarra.Add(registroNuevo);
 

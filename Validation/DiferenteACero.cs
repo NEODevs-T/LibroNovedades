4ba2e47ea4e4b687f7864aa9526948c7ff299516
Humberto7913 <humbertoalfonso8@gmail.com>
2025-07-22 16:17:25 -0400

    Se actualizaron los metodos de DiferenteACero y MayoACero para que muestren el mensaje en ingles

--- Validation/DiferenteACero.cs
+++ Validation/DiferenteACero.cs
@@ -6,16 +6,34 @@ namespace LibroNovedades.Validate
 {
     public class ValidDiferenteACero : ValidationAttribute
     {
-        public string GetErrorMessage() => $"Debe colocar un valor";
+        private string GetErrorMessage(int idPais)
+        {
+            if (idPais == 5)
+                return "You must enter a value";
+            return "Debe colocar un valor";
+        }
 
         protected override ValidationResult IsValid(object value, ValidationContext validationContext)
         {
-            if(value != null){
-                if(value.ToString() != "0"){
+            int idPais = 0;
+            var idPaisProp = validationContext.ObjectType.GetProperty("IdPais");
+            if (idPaisProp != null)
+            {
+                var val = idPaisProp.GetValue(validationContext.ObjectInstance);
+                if (val != null && int.TryParse(val.ToString(), out int pais))
+                {
+                    idPais = pais;
+                }
+            }
+
+            if (value != null)
+            {
+                if (value.ToString() != "0")
+                {
                     return ValidationResult.Success;
                 }
             }
-            return new ValidationResult(GetErrorMessage());
+            return new ValidationResult(GetErrorMessage(idPais));
         }
     }
 }
--- Validation/MayoaACero.cs
+++ Validation/MayoaACero.cs
@@ -6,16 +6,34 @@ namespace LibroNovedades.Validate
 {
     public class ValidMayorACero : ValidationAttribute
     {
-        public string GetErrorMessage() => $"Debe colocar un valor mayor a 0";
+        private string GetErrorMessage(int idPais)
+        {
+            if (idPais == 5)
+                return "You must enter a value greater than 0";
+            return "Debe colocar un valor mayor a 0";
+        }
 
         protected override ValidationResult IsValid(object value, ValidationContext validationContext)
         {
-            if(value != null){
-                if(Int64.Parse(value.ToString()) > 0 || Int64.Parse(value.ToString()) == -1){
+            int idPais = 0;
+            var idPaisProp = validationContext.ObjectType.GetProperty("IdPais");
+            if (idPaisProp != null)
+            {
+                var val = idPaisProp.GetValue(validationContext.ObjectInstance);
+                if (val != null && int.TryParse(val.ToString(), out int pais))
+                {
+                    idPais = pais;
+                }
+            }
+
+            if (value != null)
+            {
+                if (Int64.TryParse(value.ToString(), out long valNum) && (valNum > 0 || valNum == -1))
+                {
                     return ValidationResult.Success;
                 }
             }
-            return new ValidationResult(GetErrorMessage());
+            return new ValidationResult(GetErrorMessage(idPais));
         }
     }
 }

ede73dffab0f5c248bab5531108b60b0cbee4492
Humberto Vivas <humberto.vivas@papeleslatinos.com>
2025-12-22 10:35:53 -0400

    Se crearon las dto para Gespline y se modificaron las url de los metodos que llaman a este. Tambien, se cambio a sk 8.0

---
+++ DTO/GesplineDTOs/ParadasActualesAgrupadasDTO.cs
@@ -0,0 +1,14 @@
+
+using System;
+using System.Collections.Generic;
+
+namespace LibroNovedades.DTOs;
+public class ParadasActualesAgrupadasDTO
+{
+    public string CodigoParada { get; set; }
+    public string CodigoGrupoParada { get; set; }
+    public string ACodGes { get; set; }
+    public string NombreParada { get; set; }
+    public string Aparte { get; set; }
+    public double TiempoPerdido { get; set; }
+}
